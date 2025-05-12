725ddb2826fc8c7a6ec30edb7482fef663d18729
urwashi-lav <ulavsare@gmail.com>
2025-05-12 23:10:18 +0530

    terraform configuration file added

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0b7de67988bf1a282" # Replace with your desired AMI
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "GitHubActionsEC2"
+  }
+}
