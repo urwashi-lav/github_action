ea9d670870d4c5b3099fbb594195b66403423558
urwashi-lav <ulavsare@gmail.com>
2025-05-13 16:04:00 +0530

    added backend config

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket         = "gitaction-backend"
+    region         = "us-east-1"
+    key            = "gitaction-backend/terraform.tfstate"
+    encrypt        = true
+  }
+}
