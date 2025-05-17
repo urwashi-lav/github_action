437de62b3c9f723e7884014b50156b2e509b9b3f
urwashi-lav <ulavsare@gmail.com>
2025-05-17 16:08:06 +0530

    modified backend.tf for dynamodb table

--- backend.tf
+++ backend.tf
@@ -4,6 +4,6 @@ terraform {
     key            = "terraform/terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true
-    dynamodb_table = "terraform-lock-table"
+    dynamodb_table = "terraform-my-lock-table"
   }
 }
