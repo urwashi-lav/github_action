de3ada4d9e3928bf56e6bc7c6813355021821af0
urwashi-lav <ulavsare@gmail.com>
2025-05-21 00:09:37 +0530

    add keypair to ec2

--- main.tf
+++ main.tf
@@ -5,8 +5,14 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-0b7de67988bf1a282" # Replace with your desired AMI
   instance_type = "t2.micro"
+  key_name      = aws_key_pair.web-host.key_name
 
   tags = {
     Name = "GitHubActionsEC2-test1"
   }
 }
+resource "aws_key_pair" "web-host" {
+  key_name   = "web-host"
+  public_key = file("C:/Users/Acer/.ssh/id_ed25519.pub")
+}
+
