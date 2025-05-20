0f208f0512acee13a391b7985af86f1139c2c13e
urwashi-lav <ulavsare@gmail.com>
2025-05-21 01:37:05 +0530

    allow ssh and http

--- main.tf
+++ main.tf
@@ -3,12 +3,45 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0b7de67988bf1a282" # Replace with your desired AMI
-  instance_type = "t2.micro"
-  key_name      = aws_key_pair.web-host.key_name
+  ami             = "ami-0b7de67988bf1a282" # Replace with your desired AMI
+  instance_type   = "t2.micro"
+  key_name        = aws_key_pair.web-host.key_name
+  security_groups = [aws_security_group.web_sg.name]
 
   tags = {
     Name = "GitHubActionsEC2-test1"
+  }
+}
+resource "aws_security_group" "web_sg" {
+  name        = "web-server-sg"
+  description = "Allow SSH and HTTP"
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow from anywhere (limit to IP for better security)
+  }
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web-sg"
   }
 }
 resource "aws_key_pair" "web-host" {
