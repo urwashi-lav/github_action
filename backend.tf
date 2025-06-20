004a167a53f6ba0e58a753b23b2e5d10343a809b
urwashi-lav <ulavsare@gmail.com>
2025-06-20 16:16:57 +0530

    change lock table

--- backend.tf
+++ backend.tf
@@ -4,6 +4,6 @@ terraform {
     key            = "github/terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true
-    dynamodb_table = "terraformurvi-lock-table"
+    dynamodb_table = "git-lock-table"
   }
 }
